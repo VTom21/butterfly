d6a478c648bae5e885f2acafa047b83a0775bd12
Mundapolis <100851314+VTom21@users.noreply.github.com>
2026-01-23 19:47:53 +0100

    Basic Colors & Hues

---
+++ src/js/tags.js
@@ -0,0 +1,44 @@
+
+function Break() {
+  document.querySelectorAll("break").forEach(b => {
+    const div = document.createElement("br");
+    b.parentNode?.replaceChild(div, b);
+  });
+}
+
+
+function Extra_Break(counter = 2) {
+  document.querySelectorAll("extra-break").forEach(b => {
+    const fragment = document.createDocumentFragment();
+    for(let i = 0; i < counter; i++){
+        const div = document.createElement("br");
+        fragment.appendChild(div);
+    }
+    b.parentNode?.replaceChild(fragment, b);
+  });
+}
+
+function Custom_Break() {
+  document.querySelectorAll("custom-break").forEach(b => {
+    const fragment = document.createDocumentFragment();
+    const howMany = parseInt(b.getAttribute("n") || "0");
+    for(let i = 0; i < howMany; i++){
+        const div = document.createElement("br");
+        fragment.appendChild(div);
+    }
+    b.parentNode?.replaceChild(fragment, b);
+  });
+}
+
+document.querySelectorAll('.rainbow').forEach(el => {
+  const offset = el.getAttribute('offset');
+  if (offset) {
+    el.style.setProperty('--offset', offset);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  Break(); 
+  Extra_Break();
+  Custom_Break();
+});
