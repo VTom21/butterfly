565e3e84d7e65fd385bdf3a7361a402e632321bd
Visegrádi Tamás <72553156301@szentgyorgyi.internal>
2026-02-19 12:00:22 +0100

    test

--- src/js/index.ts
+++ src/js/index.ts
@@ -2,5 +2,5 @@ import "./tags.js";
 import "./attributes.js";
 import "./events.js";
 import "./macros.js";
-import "./other.js";
+import "./other.ts";
 import "./fetch.js";
--- src/js/other.ts
+++ src/js/other.ts
@@ -3,14 +3,14 @@
 document.querySelectorAll(".dropdown-toggle").forEach(btn =>{
     btn.addEventListener('click', e =>{
         const dropdown = btn.parentElement;
-
+        if(!dropdown){return};
         dropdown.classList.toggle("show");
     });
 });
 
 document.addEventListener('click', e => {
   document.querySelectorAll('.dropdown').forEach(dropdown => {
-    if (!dropdown.contains(e.target)) {
+    if (!dropdown.contains(e.target as Node)) {
       dropdown.classList.remove('show');
     }
   });
